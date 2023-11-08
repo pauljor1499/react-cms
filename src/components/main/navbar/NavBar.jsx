c5822fc112cc308f2c2bd534508e3ad4e6dce4ee
Paul Jor <jorpaulvincent@gmail.com>
2023-11-08 22:45:19 +0400

    add react mui

--- src/components/main/navbar/NavBar.jsx
+++ src/components/main/navbar/NavBar.jsx
@@ -1,37 +1,41 @@
 import React from "react";
 import "./NavBar.css";
 import { NavLink, useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 const NavBar = () => {
     const navigate = useNavigate();
 
     return (
         <div className="navbar-content">
-            <div className="content-title">
-                <h2>CMS</h2>
-            </div>
             <div className="content-items">
+                <div className="item-logo">
+                    <h2>MathMatters</h2>
+                </div>
                 <div className="item-links">
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/about">About</NavLink>
                     <NavLink to="/class">Class</NavLink>
                 </div>
-                <div className="item-buttons">
-                    <button
-                        onClick={() => {
-                            navigate("/login");
-                        }}
-                    >
-                        Login
-                    </button>
-                    <button
-                        onClick={() => {
-                            navigate("/register");
-                        }}
-                    >
-                        Register
-                    </button>
-                </div>
+            </div>
+            <div className="item-buttons">
+                <Button
+                    variant="outlined"
+                    onClick={() => {
+                        navigate("/login");
+                    }}
+                >
+                    Login
+                </Button>
+
+                <Button
+                    variant="contained"
+                    onClick={() => {
+                        navigate("/register");
+                    }}
+                >
+                    Register
+                </Button>
             </div>
         </div>
     );
