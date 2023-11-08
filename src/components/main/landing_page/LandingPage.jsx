81277ae695b8df9a84767b0979f891744cc422ef
Paul Jor <jorpaulvincent@gmail.com>
2023-11-09 00:07:19 +0400

    add landing page section 3

--- src/components/main/landing_page/LandingPage.jsx
+++ src/components/main/landing_page/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import FirstSection from "./sections/first_section/FirstSection";
 import SecondSection from "./sections/second_section/SecondSection";
+import ThirdSection from "./sections/third_section/ThirdSection";
 
 const LandingPage = () => {
     return (
         <div>
             <FirstSection />
             <SecondSection />
+            <ThirdSection />
         </div>
     );
 };
---
+++ src/components/main/landing_page/sections/third_section/ThirdSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ThirdSection = () => {
+    return (
+        <div>
+            <h4>Section 3</h4>
+        </div>
+    );
+};
+
+export default ThirdSection;
