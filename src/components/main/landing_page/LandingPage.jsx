2c53f31dfdbaaee3a4dce7bd8072ae9495aa25b3
Paul Jor <jorpaulvincent@gmail.com>
2023-11-09 18:08:34 +0400

    add landing page section 5

--- src/components/main/landing_page/LandingPage.jsx
+++ src/components/main/landing_page/LandingPage.jsx
@@ -3,6 +3,7 @@ import FirstSection from "./sections/first_section/FirstSection";
 import SecondSection from "./sections/second_section/SecondSection";
 import ThirdSection from "./sections/third_section/ThirdSection";
 import FourthSection from "./sections/fourth_section/FourthSection";
+import FifthSection from "./sections/fifth_section/FifthSection";
 
 const LandingPage = () => {
     return (
@@ -11,6 +12,7 @@ const LandingPage = () => {
             <SecondSection />
             <ThirdSection />
             <FourthSection />
+            <FifthSection />
         </div>
     );
 };
---
+++ src/components/main/landing_page/sections/fifth_section/FifthSection.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./FifthSection.css";
+
+const theme = createTheme({
+    palette: {
+        ochre: {
+            main: "#FFF",
+            light: "#FFF",
+            dark: "#A9A9A9",
+            contrastText: "black",
+            // light: will be calculated from palette.primary.main,
+            // dark: will be calculated from palette.primary.main,
+            // contrastText: will be calculated to contrast with palette.primary.main
+        },
+    },
+});
+
+const FifthSection = () => {
+    return (
+        <div className="fifth-section-content">
+            <div className="content-title">
+                <h1>Create online classrooms and train your learners easily and effectively.</h1>
+                <ThemeProvider theme={theme}>
+                    <Button variant="contained" size="large" color="ochre">
+                        Get Started
+                    </Button>
+                </ThemeProvider>
+            </div>
+        </div>
+    );
+};
+
+export default FifthSection;
