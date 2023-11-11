0f72f569905d80da1fe1de4f12fd723695630953
Paul Jor <jorpaulvincent@gmail.com>
2023-11-11 17:39:06 +0400

    update login page

--- src/components/main/login/Login.jsx
+++ src/components/main/login/Login.jsx
@@ -1,95 +1,103 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
+import React from "react";
+import "./Login.css";
+//card
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+// import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
+//text field
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import FormHelperText from "@mui/material/FormHelperText";
+//password
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+//select
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 
-function Copyright(props) {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {"Copyright © "}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
-}
-
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
-
-export default function Login() {
-    const handleSubmit = (event) => {
+const Login = () => {
+    const [showPassword, setShowPassword] = React.useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleMouseEvents = (event) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get("email"),
-            password: data.get("password"),
-        });
+    };
+
+    const [role, setRole] = React.useState("");
+    const handleRoleSelect = (event) => {
+        setRole(event.target.value);
     };
 
     return (
-        <ThemeProvider theme={defaultTheme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
-                        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-        </ThemeProvider>
+        <>
+            <div className="login-page-content">
+                <Card className="content-card">
+                    <CardContent>
+                        <div className="card-title">
+                            <h4>Log In Account</h4>
+                        </div>
+                        <br />
+                        <br />
+                        <br />
+                        <div className="card-body">
+                            <FormControl fullWidth required margin="normal">
+                                <InputLabel id="demo-simple-select-helper-label">Role</InputLabel>
+                                <Select value={role} onChange={handleRoleSelect} label="Role">
+                                    <MenuItem value={"Student"}>Student</MenuItem>
+                                    <MenuItem value={"Teacher"}>Teacher</MenuItem>
+                                </Select>
+                                {/* <FormHelperText>With label + helper text</FormHelperText> */}
+                            </FormControl>
+                            <br />
+                            <br />
+                            <br />
+                            <TextField
+                                label="Email"
+                                variant="outlined"
+                                margin="none"
+                                required
+                                fullWidth
+                            />
+                            <br />
+                            <br />
+                            <br />
+                            <FormControl fullWidth required>
+                                <InputLabel>Password</InputLabel>
+                                <OutlinedInput
+                                    label="Password"
+                                    required
+                                    type={showPassword ? "text" : "password"}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseEvents}
+                                                onMouseUp={handleMouseEvents}
+                                                edge="end"
+                                            >
+                                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                            <br />
+                            <br />
+                            <br />
+                            <Button variant="contained" fullWidth>
+                                <b>Log In</b>
+                            </Button>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
     );
-}
+};
+
+export default Login;
