18aba447f1ef95d65650b502dd7a55567fba7c1a
Paul Jor <jorpaulvincent@gmail.com>
2023-11-12 00:53:12 +0400

    update page not found

--- src/components/main/page_error/PageError.jsx
+++ src/components/main/page_error/PageError.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./PageError.scss";
 
 const PageError = () => {
     return (
-        <div>
-            <h2>Page Not Found</h2>
+        <div className="page-error-content">
+            <h2>404 - PAGE NOT FOUND</h2>
+            <p>
+                The page you are looking for might have been removed, changed, or temporarily
+                unavailable.
+            </p>
         </div>
     );
 };
