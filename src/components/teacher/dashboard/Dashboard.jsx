0bbb3a1f4e9d2fffec93c15f73bcba7dfdc56bf8
Paul Jor <jorpaulvincent@gmail.com>
2023-11-12 10:27:23 +0400

    add teacher's dashboard

---
+++ src/components/teacher/dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Dashboard.scss";
+
+const Dashboard = () => {
+    return <div>Teacher's Dashboard</div>;
+};
+
+export default Dashboard;
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//main page
 import MainPage from "../components/main/Main";
 import LandingPage from "../components/main/landing_page/LandingPage";
 import Login from "../components/main/login/Login";
@@ -6,6 +7,10 @@ import Register from "../components/main/register/Register";
 import AboutPage from "../components/main/about_page/AboutPage";
 import ClassPage from "../components/main/class_page/ClassPage";
 import PageError from "../components/main/page_error/PageError";
+
+//teacher page
+import TeacherMainPage from "../components/teacher/Main";
+import TeacherDashboard from "../components/teacher/dashboard/Dashboard.jsx";
 
 const routes = createBrowserRouter([
     {
@@ -35,6 +40,16 @@ const routes = createBrowserRouter([
             {
                 path: "*",
                 element: <PageError />,
+            },
+        ],
+    },
+    {
+        path: "teacher",
+        element: <TeacherMainPage />,
+        children: [
+            {
+                path: "",
+                element: <TeacherDashboard />,
             },
         ],
     },
